34045550f492768b923db7cab2f1245a83688a3e
table <cytrynek106@gmail.com>
2024-02-12 18:44:12 +0100

    Log functions now return None

--- buckshot/log.py
+++ buckshot/log.py
@@ -2,16 +2,16 @@ import logging
 logging.basicConfig(level=logging.INFO, filename="debug.log", filemode="w", format="@ line %(lineno)d - %(message)s")
 
 def debug(msg):
-    return logging.debug(msg)
+    logging.debug(msg)
 
 def info(msg):
-    return logging.info(msg)
+    logging.info(msg)
 
 def warning(msg):
-    return logging.warning(msg)
+    logging.warning(msg)
 
 def error(msg):
-    return logging.error(msg)
+    logging.error(msg)
 
 def critical(msg):
-    return logging.critical(msg)
+    logging.critical(msg)
