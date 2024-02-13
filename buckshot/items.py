5826c5c43be1072365963c1f56abc776e1fd8420
table <cytrynek106@gmail.com>
2024-02-13 16:07:58 +0100

    Fixed items not using up

--- buckshot/items.py
+++ buckshot/items.py
@@ -1,25 +1,28 @@
-def useBeer(shotgun: object):
+def useBeer(player: object, shotgun: object):
     print(f"you unloaded a {shotgun.content[0]} shell.")
-    try:
-        shotgun.shoot()
-    except IndexError:
-        print('ATTEMPTED TO UNLOAD EMPTY SHOTGUN')
+    shotgun.shoot()
+    player.inv.remove("beer")
 
-def useKnife(shotgun: object):
-    shotgun.dmg = 2
+def useKnife(player: object, shotgun: object):
     print("the next shot will deal 2 damage.")
+    shotgun.dmg = 2
+    player.inv.remove("knife")
 
-def useGlass(shotgun: object):
+def useGlass(player: object, shotgun: object):
     print(f"the shell in the chamber is a {shotgun.content[0]} one")
+    player.inv.remove("magnifying glass")
+    
 
 def useCigarette(player: object):
     if player.lives == player.lifeCap:
         print("you already have max lives.")
     else:
         player.heal()
+    player.inv.remove("cigarette")
 
 def useCuffs(player: object):
     if player.cuffed:
         print("they're already cuffed. (item not consumed)")
     else:
         player.cuffed = 1
+        player.inv.remove("cuffs")
