bde30b6f652d4b3c9c9af65f08d050bda9e23ba5
table <cytrynek106@gmail.com>
2024-02-13 21:26:37 +0100

    Changed the log format

--- buckshot/log.py
+++ buckshot/log.py
@@ -1,5 +1,5 @@
 import logging
-logging.basicConfig(level=logging.INFO, filename="debug.log", filemode="w", format="at line %(lineno)d - %(message)s")
+logging.basicConfig(level=logging.INFO, filename="debug.log", filemode="w", format="%(asctime)s at line no. %(lineno)d - %(message)s")
 
 def debug(msg):
     logging.debug(msg)
@@ -17,4 +17,4 @@ def critical(msg):
     logging.critical(msg)
 
 def weLoggingAllOfIt():
-    logging.basicConfig(level=logging.DEBUG, filename="debug.log", filemode="w", format="at line %(lineno)d - %(message)s")
+    logging.basicConfig(level=logging.DEBUG, filename="debug.log", filemode="w", format="%(asctime)s at line no. %(lineno)d - %(message)s")
