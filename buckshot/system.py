fc408a04e7056463e1f7a74391ece3eb377a11b7
table106 <cytrynek106@gmail.com>
2024-02-20 18:05:26 +0100

    Logging gets imported in every lib

--- buckshot/system.py
+++ buckshot/system.py
@@ -1,7 +1,8 @@
 from os import system
 from time import sleep
 
-import log
+import logging
+logging.basicConfig(level=logging.DEBUG, filename="debug.log", filemode="w", format="%(asctime)s at line no. %(lineno)d - %(message)s")
 
 def clear() -> None:
     system("cls")
@@ -17,7 +18,7 @@ def testmodeLog(plr1: object, plr2: object, shotgun: object, testmode: bool=Fals
         print(f"1: {plr1.__repr__()}\
               \n2: {plr2.__repr__()}\
               \nshotgun: {shotgun}")
-        log.debug(f"plr1 state: {plr1.__repr__()}\
+        logging.debug(f"plr1 state: {plr1.__repr__()}\
               \nplr2 state: {plr2.__repr__()}\
               \nshotgun: {shotgun}")
     if testmode:
