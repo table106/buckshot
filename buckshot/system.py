36e566c01dc57cb43869fed6b2e5436f852fa747
table106 <cytrynek106@gmail.com>
2024-02-20 18:39:21 +0100

    Player 3 preparations

--- buckshot/system.py
+++ buckshot/system.py
@@ -8,21 +8,16 @@ def clear() -> None:
     system("cls")
 
 def trySleep(duration: int, testmode: bool=False) -> None:
-    if testmode:
-        return None
-    else:
+    if not testmode:
         sleep(duration)
 
-def testmodeLog(plr1: object, plr2: object, shotgun: object, testmode: bool=False) -> None:
-    def printAndLogInfo() -> None:
-        print(f"1: {plr1.__repr__()}\
-              \n2: {plr2.__repr__()}\
-              \nshotgun: {shotgun}")
-        logging.debug(f"plr1 state: {plr1.__repr__()}\
-              \nplr2 state: {plr2.__repr__()}\
-              \nshotgun: {shotgun}")
+def testmodeLog(shotgun: object, testmode: bool=False, *players) -> None:
     if testmode:
-        printAndLogInfo()
+        for j,player in enumerate(players):
+            print(f"{j+1}: {player.__repr__()}")
+            logging.debug(f"plr {j+1} state: {player.__repr__()}")
+        print(f"shotgun: {shotgun}")
+        logging.debug(f"shotgun: {shotgun}")
 
 if __name__ == "__main__":
     print("wrong file idiot")
