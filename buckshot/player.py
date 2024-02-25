46999fb1afb695b6ca491945c39336250091905b
table <cytrynek106@gmail.com>
2024-02-25 12:43:52 +0100

    Grammer

--- buckshot/player.py
+++ buckshot/player.py
@@ -39,7 +39,7 @@ class Player:
     
     def turn(self, shotgun: Shotgun):
         print(self)
-        ans = input("say to use:\nshotgun - shoot\n>")
+        ans = input("type to use:\nshotgun - shoot\n>")
         match (ans):
             case "shoot":
                 ans = input("shoot self or enemy?\n>")
