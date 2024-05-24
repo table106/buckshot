4a318d75c2136920808889fbce5f508e7f44dbae
table <cytrynek106@gmail.com>
2024-05-24 21:00:12 +0200

    initOpponents() now has forced positional args

--- buckshot/system.py
+++ buckshot/system.py
@@ -6,7 +6,7 @@ def clear() -> None:
 def validName(name: str, *players: tuple[str]) -> bool:
     return (name not in players and name != "" and name != " ")
 
-def initOpponents(plr1: object, plr2: object, plr3: object=None) -> None:
+def initOpponents(plr1: object, plr2: object, plr3: object=None, /) -> None:
     if plr3 != None:
         plr1.addOpponents(plr2, plr3)
         plr2.addOpponents(plr1, plr3)
